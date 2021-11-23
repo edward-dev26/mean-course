1978af6fe579f5170fc9d64febe355ce1b836529
Eduard Shvetsov <eshvetsov@griddynamics.com>
2021-11-23 09:59:03 +0200

    Release 0.0.1

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+console.log('V0.0.1');
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
